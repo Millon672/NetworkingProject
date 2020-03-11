9d57b48c90136fa256e40bf0c4ba13b12a866892
Millon672 <61213292+Millon672@users.noreply.github.com>
2020-03-11 12:29:36 -0400

    Update client.py
    Fixing comments

--- client.py
+++ client.py
@@ -18,14 +18,10 @@ while True:
     # maintains a list of possible input streams
     sockets_list = [sys.stdin, server]
   
-    """ There are two possible input situations. Either the
-    user wants to give  manual input to send to other people,
+    """The user gives  manual input to send to other people,
     or the server is sending a message  to be printed on the
     screen. Select returns from sockets_list, the stream that
-    is reader for input. So for example, if the server wants
-    to send a message, then the if condition will hold true
-    below.If the user wants to send a message, the else
-    condition will evaluate as true"""
+    is reader for input."""
     read_sockets,write_socket, error_socket = select.select(sockets_list,[],[])
   
     for socks in read_sockets:
