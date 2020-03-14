dbcd38a8c3f2e4bf771cce26bc87b666defe4c80
Bryson Chavis <49242436+zepthro@users.noreply.github.com>
2020-03-14 16:42:23 -0400

    Add files via upload
    Added a side bar. Also, added enter feature for send.

--- GUI_driver.py
+++ GUI_driver.py
@@ -11,16 +11,20 @@ class Window(wx.Frame):
         vbox = wx.BoxSizer(wx.VERTICAL) 
         # add a text box to the screen
         self.text_read = wx.TextCtrl(self,size=(WIDTH,HEIGHT/2))
+        self.text_out  = wx.StaticText(self,pos=(wx.ALIGN_BOTTOM, wx.ALIGN_LEFT),size=(WIDTH, HEIGHT/2))
         self.send_button = wx.Button(self,label="Send", pos=(0,wx.ALIGN_RIGHT))
         self.send_button.Bind(wx.EVT_BUTTON, self.OnToggle)
+
+        self.Bind(wx.EVT_KEY_DOWN, self.OnToggle)
         self.Show(True)
 
     def OnToggle(self, event):
         # Handle the event in here.
         # read the user input from the button.
-
+        self.text_out.SetLabelText(self.text_out.GetLabelText()
+        + "\n" + "<" + "You" + "> " + self.text_read.GetValue())
         print (self.text_read.GetValue())
-        
+
         #clear the text that is sitting in the buffer
         self.text_read.SetValue("")
 
